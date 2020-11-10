7f2bd24d450e776ef4bd98768c6eaedbb04f8551
Amos Elmaliah <amosel@gmail.com>
2020-11-10 00:32:59 -0500

    remove traced of codepush.

--- app/components/version-view.tsx
+++ app/components/version-view.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import {Text} from 'react-native';
-import styles, {colors} from '../styles';
-import {useStore} from '../provider';
+import React from "react";
+import { Text } from "react-native";
+import styles, { colors } from "../styles";
+import { useStore } from "../provider";
+import { EnvType } from "../store/env.mst";
 
 export function VersionView() {
   const store = useStore();
-  const {env} = store;
+  const { env } = store!;
 
   if (__DEV__) {
     return (
-      <Text style={[styles.body, {color: colors.BUTTONBGCOLOR}]}>
+      <Text style={[styles.body, { color: colors.BUTTONBGCOLOR }]}>
         Debug Version
       </Text>
     );
-  } else if (!env.codepush) {
+  } else if (!env.expo.version) {
     return (
-      <Text style={[styles.body, {color: colors.BUTTONBGCOLOR}]}>
-        Unknown Code Push Version
+      <Text style={[styles.body, { color: colors.BUTTONBGCOLOR }]}>
+        Unknown Expo Version
       </Text>
     );
   }
   return (
-    <Text style={[styles.body, {color: colors.BUTTONBGCOLOR}]}>
-      {`Code Push Version: ${env.codepush.appVersion}`}
+    <Text style={[styles.body, { color: colors.BUTTONBGCOLOR }]}>
+      {`Expo Version: ${env.expo.version}`}
     </Text>
   );
 }
